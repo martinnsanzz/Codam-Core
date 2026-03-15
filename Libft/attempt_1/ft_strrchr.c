20217d6cab3c44a4edeab5813009f37ddaf2d534
Martin Sanz Sanchez <martinuco569@gmail.com>
2026-03-15 19:20:55 +0100

    Modified Makefile to be inside testing file Renamed test_func file to testing Finished strrchr

--- Libft/attempt_1/ft_strrchr.c
+++ Libft/attempt_1/ft_strrchr.c
@@ -6,7 +6,7 @@
 /*   By: 2002mssm02 <2002mssm02@student.42.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/10 09:40:40 by masanz-s          #+#    #+#             */
-/*   Updated: 2026/03/13 14:37:18 by 2002mssm02       ###   ########.fr       */
+/*   Updated: 2026/03/15 19:15:58 by 2002mssm02       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,19 @@
 
 char	*ft_strrchr(const char *s, int c)
 {
-	
+	int		len;
+	char	*str;
+
+	c = (char)c;
+	str = (char *)s;
+	len = ft_strlen(str);
+	if (c == str[len])
+		return (&str[len]);
+	while (len >= 0)
+	{
+		if (c == str[len])
+			return (&str[len]);
+		len--;
+	}
+	return (NULL);
 }
