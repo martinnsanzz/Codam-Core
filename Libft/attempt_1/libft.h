f1f1f7c7616746beacd89661c90c01623faed6ca
Martin Sanz Sanchez <martinuco569@gmail.com>
2026-03-15 23:02:56 +0100

    Fixed double prototype of memcmp

--- Libft/attempt_1/libft.h
+++ Libft/attempt_1/libft.h
@@ -6,7 +6,7 @@
 /*   By: 2002mssm02 <2002mssm02@student.42.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/10 09:18:34 by masanz-s          #+#    #+#             */
-/*   Updated: 2026/03/13 14:22:31 by 2002mssm02       ###   ########.fr       */
+/*   Updated: 2026/03/15 23:02:23 by 2002mssm02       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -43,7 +43,6 @@ char			*ft_strchr(const char *s, int c);
 char			*ft_strrchr(const char *s, int c);
 int				ft_strncmp(const char *s1, const char *s2, size_t n);
 void			*ft_memchr(const void *s, int c, size_t n);
-int				ft_memcmp(const void *s1, const void *s2, size_t n);
 int				ft_memcmp(const void *s1, const void *s2, size_t n);
 int				ft_atoi(const char *nptr);
 void			*ft_calloc(size_t nmemb, size_t size);
