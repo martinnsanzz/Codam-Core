f1594c6571b5d31433f46767538a16cabc5785a8
masanz-s <masanz-s@student.codam.nl>
2026-04-08 14:31:14 +0200

    Fixed norminette

--- Milestone_1/Push_Swap/push_swap.h
+++ Milestone_1/Push_Swap/push_swap.h
@@ -1,25 +1,25 @@
 #ifndef PUSH_SWAP_H
 # define PUSH_SWAP_H
 
-#include "libft/libft.h"
+# include "libft/libft.h"
 
 typedef enum e_strategy
 {
-    ADAPTIVE,
-    SIMPLE,
-    MEDIUM,
-    COMPLEX
-}   t_strategy;
+	ADAPTIVE,
+	SIMPLE,
+	MEDIUM,
+	COMPLEX
+}	t_strategy;
 
 typedef struct s_flags
 {
-    t_strategy  strategy;
-    int         bench;
+	t_strategy	strategy;
+	int			bench;
 	int			n_flags;
-}   t_flags;
+}	t_flags;
 
 //Argument validation functions
-void check_argv(int argc, char *argv[], t_flags *flags);
+void	check_argv(int argc, char *argv[], t_flags *flags);
 
 //Utils functions
 void	print_error(void);
@@ -27,4 +27,3 @@ long	ft_atoi_strict(const char *nptr);
 int		ft_strcmp(const char *s1, const char *s2);
 
 #endif
- 
