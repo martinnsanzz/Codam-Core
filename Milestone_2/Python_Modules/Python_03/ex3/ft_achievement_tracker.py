7d1b6c414c5448c0dacdeb1ed4c153e11dfa6c11
Martin Sanz Sanchez <martinuco569@gmail.com>
2026-05-21 12:05:31 +0200

    Finished ex3

--- Milestone_2/Python_Modules/Python_03/ex3/ft_achievement_tracker.py
+++ Milestone_2/Python_Modules/Python_03/ex3/ft_achievement_tracker.py
@@ -28,15 +28,62 @@ ACHIEVEMENT_LIST = [
 ]
 
 
+class Colors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKCYAN = '\033[96m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
+
 def gen_player_achievements() -> set:
-    num_elements: int = random.randint(0, len(ACHIEVEMENT_LIST))
+    num_elements: int = random.randint(1, len(ACHIEVEMENT_LIST))
     rand_set: set = set(random.sample(ACHIEVEMENT_LIST, k=num_elements))
 
     return rand_set
 
 
+def display_player_info(player: set, player_name: str) -> None:
+    print(Colors.OKGREEN + f"Player {player_name}: " + Colors.ENDC, end="")
+    print(player)
+
+
 if __name__ == "__main__":
-    Alice: set = gen_player_achievements()
-    Bob: set = gen_player_achievements()
-    Charlie: set = gen_player_achievements()
-    Dylan: set = gen_player_achievements()
+    alice: set = gen_player_achievements()
+    bob: set = gen_player_achievements()
+    charlie: set = gen_player_achievements()
+    dylan: set = gen_player_achievements()
+
+    print(Colors.HEADER + "=== Achievement Tracker System ===", end="\n\n")
+    display_player_info(alice, "Alice")
+    display_player_info(bob, "Bob")
+    display_player_info(charlie, "Charlie")
+    display_player_info(dylan, "Dylan")
+
+    print(Colors.OKBLUE + "\nAll distinct achievements: ", end="")
+    print(Colors.ENDC + f"{set.union(alice, bob, charlie, dylan)}", end="\n\n")
+
+    print(Colors.OKBLUE + "\nCommon achievements: " + Colors.ENDC, end="")
+    print(set.intersection(alice, bob, charlie, dylan), end="\n\n")
+
+    print(Colors.OKCYAN + "Only Alice has: " + Colors.ENDC, end="")
+    print(set.difference(alice, bob, charlie, dylan))
+    print(Colors.OKCYAN + "Only Bob has: " + Colors.ENDC, end="")
+    print(set.difference(bob, alice, charlie, dylan))
+    print(Colors.OKCYAN + "Only Charlie has: " + Colors.ENDC, end="")
+    print(set.difference(charlie, bob, alice, dylan))
+    print(Colors.OKCYAN + "Only Dylan has: " + Colors.ENDC, end="")
+    print(set.difference(dylan, bob, charlie, alice))
+
+    print(Colors.OKCYAN + f"\nAlice '{len(alice)}' is missing: ", end="")
+    print(Colors.ENDC + f"{set.difference(set(ACHIEVEMENT_LIST), alice)}")
+    print(Colors.OKCYAN + f"Bob '{len(bob)}' is missing: ", end="")
+    print(Colors.ENDC + f"{set.difference(set(ACHIEVEMENT_LIST), bob)}")
+    print(Colors.OKCYAN + f"Charlie '{len(charlie)}' is missing: ", end="")
+    print(Colors.ENDC + f"{set.difference(set(ACHIEVEMENT_LIST), charlie)}")
+    print(Colors.OKCYAN + f"Dylan '{len(dylan)}' is missing: ", end="")
+    print(Colors.ENDC + f"{set.difference(set(ACHIEVEMENT_LIST), dylan)}")
