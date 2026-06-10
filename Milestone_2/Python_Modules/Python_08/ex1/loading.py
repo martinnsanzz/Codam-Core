c416ddf98def868b6858146ec3cca43755307f1a
masanz-s <masanz-s@student.codam.nl>
2026-06-10 17:06:41 +0200

    Finish ex1

--- Milestone_2/Python_Modules/Python_08/ex1/loading.py
+++ Milestone_2/Python_Modules/Python_08/ex1/loading.py
@@ -41,7 +41,6 @@ except (ModuleNotFoundError, ImportError) as error:
 try:
     np = None
     import numpy as np  # type: ignore
-    import numpy.typing as npt
 except (ModuleNotFoundError, ImportError) as error:
     TOTAL_ERRORS += 1
     C().msg("F", f"     -{str(error)}")
@@ -77,10 +76,13 @@ access ready")
 ready")
 
 
-def generate_matrix_data() -> npt.NDArray:
-    if np is None:
-        raise ModuleNotFoundError("numpy not available")
-    return np.random.randn(5, 10)
+def get_data() -> dict[str, list]:
+    url = "https://api.coingecko.com/api/v3/coins/bitcoin/market_chart"
+    params = {"vs_currency": "usd",
+              "days": "30"}
+    response = requests.get(url, params=params)
+    data = response.json()
+    return data['prices']
 
 
 if __name__ == "__main__":
@@ -88,21 +90,39 @@ if __name__ == "__main__":
         install_instructions()
     dependencies_info()
 
-    if pd and np and requests and plt:
+    if TOTAL_ERRORS == 0:
         C().msg("H", "\n=== Testing dependencies ===")
         C().msg("Bo", "This uses numpy to generate an Matrix array")
-        print(generate_matrix_data())
+        array = np.random.randint(1, 100, size=[5, 5])
 
-        C().msg("Bo", "This uses panda to manipulate data")
-        data = generate_matrix_data()
-        df = pd.DataFrame(data)
+        print(f"Array: \n{array}")
+        print(f"Shape of array: {array.shape}")
+        print(f"Data type of array: {array.dtype}")
+        print(f"Size of array (n-elements): {array.size}")
+        print(f"Number of dimensions: {array.ndim}")
+
+        C().msg("Bo", "\nThis uses panda to manipulate data")
+        df = pd.read_csv('la_liga.csv')
+        C().msg("Bo", "\nData frame: ")
         print(df)
+        C().msg("Bo", "\nData frame head (first 3): ")
+        print(df.head(3))
+        C().msg("Bo", "\nData frame tail (last 3): ")
+        print(df.tail(3))
 
-        C().msg("Bo", "\nResults save to: matrix_analysis.png")
-        
-        import matplotlib.pyplot as plt  # type: ignore
-        
-        plt.hist(df.values.flatten())
-        plt.xlabel("X-Axis")
-        plt.ylabel("Y-Axis")
-        plt.savefig('matrix_analysis.png')
+        C().msg("Bo", "\nUsing requests to get data of crypto prices \
+over the last 30 days")
+        C().msg("Bo", "\nCreating crypto_analysis.png")
+        prices = get_data()
+        df = pd.DataFrame(prices, columns=['Timestamp', 'Price'])
+
+        df['moving_avg'] = df['Price'].rolling(window=7).mean()
+
+        import matplotlib.pyplot as plt  # noqa
+        plt.plot(df['Price'], label='BTC Price')
+        plt.plot(df['moving_avg'], label='7-day MA')
+
+        plt.xlabel('Data Points (30 days, ~hourly)')
+        plt.ylabel('Price (USD)')
+        plt.legend()
+        plt.savefig('crypto_analysis.png')
