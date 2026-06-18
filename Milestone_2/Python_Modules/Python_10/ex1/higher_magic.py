83427dc337544bed46cfd2b9cbb886942a22e786
Martin Sanz Sanchez <martinuco569@gmail.com>
2026-06-18 18:18:24 +0200

    Continued

--- Milestone_2/Python_Modules/Python_10/ex1/higher_magic.py
+++ Milestone_2/Python_Modules/Python_10/ex1/higher_magic.py
@@ -4,72 +4,102 @@
 from typing import Callable
 
 
+class C:
+    H = '\033[95m'  # Header
+    B = '\033[94m'  # Blue
+    C = '\033[96m'  # Cyan
+    G = '\033[92m'  # Green
+    W = '\033[93m'  # Warning
+    F = '\033[91m'  # Fail
+    E = '\033[0m'  # End
+    Bo = '\033[1m'  # Bold
+    U = '\033[4m'
+
+    def msg(self, color: str, msg: str) -> None:
+        print(getattr(self, color) + msg + C.E)
+
+
 class Grimoire:
     @staticmethod
-    def heal(target: str, power: int) -> str:
-        return f"Heal restores {target} for {power} HP"
+    def heal(t: str, p: int) -> str:
+        return f"Heal restores {t} for {p} HP"
 
     @staticmethod
-    def fireball(target: str, power: int) -> str:
-        return f"Fireball deals {power} fire damage to {target}"
+    def fireball(t: str, p: int) -> str:
+        return f"Fireball deals {p} fire damage to {t}"
 
     @staticmethod
-    def freeze(target: str, power: int) -> str:
-        return f"Freeze immobilizes {target} with {power} ice power"
+    def freeze(t: str, p: int) -> str:
+        return f"Freeze immobilizes {t} with {p} ice p"
 
     @staticmethod
-    def lightning(target: str, power: int) -> str:
-        return f"Lightning strikes {target} for {power} damage"
+    def lightning(t: str, p: int) -> str:
+        return f"Lightning strikes {t} for {p} damage"
 
     @staticmethod
-    def shield(target: str, power: int) -> str:
-        return f"Shield grants {target} {power} defense"
+    def shield(t: str, p: int) -> str:
+        return f"Shield grants {t} {p} defense"
 
     @staticmethod
-    def dark_curse(target: str, power: int) -> str:
-        return f"Dark Curse weakens {target} by {power} points"
+    def dark_curse(t: str, p: int) -> str:
+        return f"Dark Curse weakens {t} by {p} points"
+
+    @staticmethod
+    def condition(num1: int, num2: int) -> bool:
+        return True if num1 == num2 else False
 
 
 def spell_combiner(spell1: Callable[[str, int], str],
                    spell2: Callable[[str, int], str]) -> \
         Callable[[str, int], tuple[str, str]]:
-    def combined(target: str, power: int) -> tuple[str, str]:
-        return (spell1(target, power), spell2(target, power))
-    return combined
+    return lambda t, p: (spell1(t, p), spell2(t, p))
 
 
-def power_amplifier(base_spell: Callable[[str, int], str],
-                    multiplier: int) -> Callable[[str, int], str]:
-    def amplified(target: str, power: int) -> str:
-        return base_spell(target, power * multiplier)
-    return amplified
+def p_amplifier(base_spell: Callable[[str, int], str],
+                multiplier: int) -> Callable[[str, int], str]:
+    return lambda t, p: base_spell(t, p * multiplier)
 
 
-def conditional_caster(condition: Callable[[str, int], str],
+def conditional_caster(condition: Callable[[int, int], bool],
                        spell: Callable[[str, int], str]) -> \
-        Callable[[str, int], str | Callable[[str, int], str]]:
-    def condition_cast(target: str, power: int) -> \
-            str | Callable[[str, int], str]:
-        if callable(condition):
-            return spell(target, power)
-        return "Spell fizzled"
-    return condition_cast
+        Callable[[int, int, str, int], str]:
+    return lambda n1, n2, t, p: spell(t, p) if condition(n1, n2) else \
+                        "Spell fizzled"
 
 
-def spell_sequence(spells: list[Callable]) -> Callable:
-    ...
+def spell_sequence(spells: list[Callable[[str, int], str]]) -> \
+        Callable[[str, int], list[str]]:
+    return lambda t, p: [spell(t, p) for spell in spells]
 
 
 if __name__ == "__main__":
-    test_values = [7, 5, 20]
-    test_targets = ['Dragon', 'Goblin', 'Wizard', 'Knight']
+    test_target = ['Dragon', 'Goblin', 'Wizard', 'Knight']
 
+    print(C.U, end="")
+    C().msg("Bo", "Testing spell combiner:")
     combined = spell_combiner(Grimoire.dark_curse,
                               Grimoire.heal)
-    amplifier = power_amplifier(Grimoire.fireball, 5)
+    print(combined(test_target[2], 5))
 
-    cast = conditional_caster(test_values, Grimoire.shield)
+    print(C.U, end="")
+    C().msg("Bo", "\nTesting spell amplifier:")
+    amplifier = p_amplifier(Grimoire.fireball, 5)
+    print(amplifier(test_target[0], 20))
 
-    print(combined(test_targets[2], test_values[1]))
-    print(amplifier(test_targets[0], test_values[2]))
-    print(cast(test_targets[1], test_values[0]))
+    print(C.U, end="")
+    C().msg("Bo", "\nTesting conditional caster:")
+    cast = conditional_caster(Grimoire.condition, Grimoire.shield)
+    print(cast(2, 2, test_target[1], 7))
+
+    print(C.U, end="")
+    C().msg("Bo", "\nTesting spell list:")
+    spell_list: list[Callable[[str, int], str]] = [
+        Grimoire.dark_curse,
+        Grimoire.fireball,
+        Grimoire.freeze,
+        Grimoire.heal,
+        Grimoire.lightning,
+        Grimoire.shield
+    ]
+    sequence = spell_sequence(spell_list)
+    print(sequence(test_target[3], 5))
---
+++ Milestone_2/Python_Modules/Python_10/ex2/scope_mysteries.py
@@ -0,0 +1,85 @@
+#!/usr/bin/env python3
+
+
+from typing import Callable
+
+
+class C:
+    H = '\033[95m'  # Header
+    B = '\033[94m'  # Blue
+    C = '\033[96m'  # Cyan
+    G = '\033[92m'  # Green
+    W = '\033[93m'  # Warning
+    F = '\033[91m'  # Fail
+    E = '\033[0m'  # End
+    Bo = '\033[1m'  # Bold
+    U = '\033[4m'
+
+    def msg(self, color: str, msg: str) -> None:
+        print(getattr(self, color) + msg + C.E)
+
+
+def mage_counter() -> Callable[[], int]:
+    count = 0
+
+    def counter() -> int:
+        nonlocal count
+        count += 1
+        return count
+    return counter
+
+
+def spell_accumulator(initial_power: int) -> Callable[[int], int]:
+    acumulated_power = initial_power
+
+    def acumulate(power: int) -> int:
+        nonlocal acumulated_power
+        acumulated_power += power
+        return acumulated_power
+    return acumulate
+
+
+def enchantment_factory(enchantment_type: str) -> Callable[[str], str]:
+    return lambda item: enchantment_type + " " + item
+
+
+def memory_vault() -> dict[str, Callable]:
+
+    def store(key: str, value: int):
+        ...
+    
+    def recall(key: str) -> int | str:
+        ...
+    return {recall:store}
+
+
+if __name__ == "__main__":
+    print(C.U, end="")
+    C().msg("Bo", "Testing mage counter:")
+    counter_a = mage_counter()
+    counter_b = mage_counter()
+
+    print(f"counter_a call 1: {counter_a()}")
+    print(f"counter_b call 1: {counter_b()}")
+    print(f"counter_a call 2: {counter_a()}")
+
+    print(C.U, end="")
+    C().msg("Bo", "\nTesting spell acumulator:")
+    power = spell_accumulator(100)
+
+    print(f"Base 100, add 20: {power(20)}")
+    print(f"Base 100, add 30: {power(30)}")
+    print(f"Base 100, add 50: {power(50)}")
+
+    print(C.U, end="")
+    C().msg("Bo", "\nTesting enchanting items:")
+    flame_enchant = enchantment_factory("Flaming")
+    water_enchant = enchantment_factory("Respiration")
+    wind_enchant = enchantment_factory("Speed")
+
+    print("Enchanting sword with flame enchant:",
+          f"{flame_enchant("sword")}")
+    print("Enchanting helmet with water enchant:",
+          f"{water_enchant("helmet")}")
+    print("Enchanting boots with wind enchant:",
+          f"{wind_enchant("boots")}")
