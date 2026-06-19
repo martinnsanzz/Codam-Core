5a72af7dcdfcfe96d31239670d6467a760519fae
Martin Sanz Sanchez <martinuco569@gmail.com>
2026-06-19 22:24:30 +0200

    Progress

--- Milestone_2/Python_Modules/Python_10/ex4/decorator_mastery.py
+++ Milestone_2/Python_Modules/Python_10/ex4/decorator_mastery.py
@@ -2,12 +2,35 @@
 
 
 from functools import wraps
-from typing import Callable
+from typing import Callable, Any
+from time import perf_counter
+
+
+class C:
+    H = '\033[95m'  # Header
+    B = '\033[94m'  # Blue
+    C = '\033[96m'  # Cyan
+    G = '\033[92m'  # Green
+    W = '\033[93m'  # Warning
+    F = '\033[91m'  # Fail
+    E = '\033[0m'  # End
+    Bo = '\033[1m'  # Bold
+    U = '\033[4m'
+
+    def msg(self, color: str, msg: str) -> None:
+        print(getattr(self, color) + msg + C.E)
 
 
 def spell_timer(func: Callable) -> Callable:
-    ...
-
+    @wraps(func)
+    def wrapper_function(*args, **kwargs) -> Any:
+        print(f"Casting {func.__name__}")
+        elapsed_time = perf_counter()
+        result = func(*args, **kwargs)
+        elapsed_time = perf_counter() - elapsed_time
+        print(f"Spell completed in {round(elapsed_time, 3)}")
+        return result
+    return wrapper_function
 
 def power_validator(min_power: int) -> Callable:
     ...
@@ -26,5 +49,15 @@ class MageGuild:
         ...
 
 
+@spell_timer
+def slow_spell(n: int) -> int:
+    total = 0
+    for i in range(n):
+        total += i
+    return total
+
+
 if __name__ == "__main__":
-    ...
+    print(C.U, end="")
+    C().msg("Bo", "Testing spell timer...")
+    print(slow_spell(10000000))
