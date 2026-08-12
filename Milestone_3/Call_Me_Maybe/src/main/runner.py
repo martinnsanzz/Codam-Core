740c364d7380564b9ef6d68e4a59e159ac4c5027
Martin Sanz Sanchez <martinuco569@gmail.com>
2026-08-12 11:25:09 +0200

    Started testing on the printing

--- Milestone_3/Call_Me_Maybe/src/main/runner.py
+++ Milestone_3/Call_Me_Maybe/src/main/runner.py
@@ -7,11 +7,15 @@ from .classes import Prompt
 if TYPE_CHECKING:
     from llm_sdk import Small_LLM_Model
 
+MAX_TOKENS = 50
+
 def run_pipeline(small_llm: "Small_LLM_Model" ,prompts: list[Prompt],
                  functions: list[dict[str, str]]) -> None:
     prompt = prompts[0]
-    tokenization = small_llm.encode(prompt.prompt)
-    print(tokenization)
-    logits = small_llm.get_logits_from_input_ids(tokenization.tolist()[0])
-    logits.sort(reverse=False)
-    print(logits)
+    tokenization = small_llm.encode(prompt.prompt).tolist()[0]
+    for _ in range(0, MAX_TOKENS):
+        logits = small_llm.get_logits_from_input_ids(tokenization)
+        word = max(range(len(logits)), key=logits.__getitem__)
+        tokenization.append(word)
+
+    print(small_llm.decode(tokenization))
